1874eef710fa29f4a40ac79e7633aa51673051fd
JP19-03 <u202014511@upc.edu.pe>
2024-09-22 18:11:16 -0500

    feat(main): add crop-treatment-scheduler page to routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Routes } from '@angular/router';
 import {
   IrrigationSchedulerComponent
 } from "./agricultural-process/pages/irrigation-scheduler/irrigation-scheduler.component";
+import {
+  CropTreatmentSchedulerComponent
+} from "./agricultural-process/pages/crop-treatment-scheduler/crop-treatment-scheduler.component";
 
 export const routes: Routes = [
   { path: 'irrigation-scheduler', component: IrrigationSchedulerComponent },
+  { path: 'crop-treatment-scheduler', component: CropTreatmentSchedulerComponent },
 ];
