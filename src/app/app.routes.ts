b716823b88ee71fd33f52d836b850f82bc36f5a4
JP19-03 <u202014511@upc.edu.pe>
2024-09-22 21:59:55 -0500

    feat(main): add login to routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,10 @@ import {
 import {
   CropTreatmentSchedulerComponent
 } from "./agricultural-process/pages/crop-treatment-scheduler/crop-treatment-scheduler.component";
+import {LoginFormComponent} from "./profile-management/components/login-form/login-form.component";
 
 export const routes: Routes = [
   { path: 'irrigation-scheduler', component: IrrigationSchedulerComponent },
   { path: 'crop-treatment-scheduler', component: CropTreatmentSchedulerComponent },
+  { path: 'login', component: LoginFormComponent }
 ];
