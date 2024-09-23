ca20c708e9b489cd0388b946a97b3af3c447a55c
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-23 16:08:08 -0500

    feat: add irrigation table route path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import {
   IrrigationSchedulerComponent
 } from "./agricultural-process/pages/irrigation-scheduler/irrigation-scheduler.component";
 
+import {
+  IrrigationTableComponent
+} from "./agricultural-process/components/irrigation-table/irrigation-table.component";
+
 export const routes: Routes = [
   { path: 'irrigation-scheduler', component: IrrigationSchedulerComponent },
+  { path: 'irrigation-table',  component: IrrigationTableComponent }
 ];
