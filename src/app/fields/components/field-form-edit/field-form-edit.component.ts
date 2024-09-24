15be49aefcd76fb1108cf1a8167c4e1a9d2c1180
CafecitoSKR <espinofloresa@gmail.com>
2024-09-24 12:31:58 -0700

    feat(fields): added field form edit component and functions.

---
+++ src/app/fields/components/field-form-edit/field-form-edit.component.ts
@@ -0,0 +1,60 @@
+import {Component, EventEmitter, inject, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Fields} from "../../models/fields.entity";
+import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
+import {FieldsService} from "../../services/fields.service";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-field-form-edit',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    NgIf,
+    ReactiveFormsModule
+  ],
+  templateUrl: './field-form-edit.component.html',
+  styleUrl: './field-form-edit.component.css'
+})
+export class FieldFormEditComponent implements OnInit{
+  @Input() isModalOpen: boolean=true ;
+  @Input() fieldId!:number;
+  @Input() currentUserId!:number;
+  @Output() close = new EventEmitter<void>();
+  @Output() success = new EventEmitter<void>();
+  field!: Fields;
+  @ViewChild('fieldForm', { static: false}) fieldForm!: NgForm;
+  fieldService: FieldsService = inject(FieldsService);
+
+  constructor() {
+    this.field=new Fields({});
+  }
+
+  ngOnInit(): void {
+    this.field.userId= this.currentUserId;
+  }
+  private resetForm(){
+    this.fieldForm.resetForm();
+    this.field=new Fields({});
+  }
+
+  onSubmit() {
+    if (this.fieldForm.form.valid) {
+      this.fieldService.update(this.fieldId, this.field).subscribe((response: any) => {
+        console.log("Field Updated", response);
+        this.success.emit();
+        this.resetForm();
+        this.isModalOpen = false;
+      });
+    }
+  }
+  onCancel() {
+    this.resetForm();
+    this.isModalOpen = false;
+    this.close.emit();
+  }
+}
