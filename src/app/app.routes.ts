2115391cfc66896fdf00e3c1f001793710227f0e
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-09-23 21:51:37 -0500

    feat: add crop treatment table path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,22 @@ import { Routes } from '@angular/router';
 import {
   IrrigationSchedulerComponent
 } from "./agricultural-process/pages/irrigation-scheduler/irrigation-scheduler.component";
+
 import {
   CropTreatmentSchedulerComponent
 } from "./agricultural-process/pages/crop-treatment-scheduler/crop-treatment-scheduler.component";
-import {LoginFormComponent} from "./profile-management/components/login-form/login-form.component";
+
+import {
+  LoginFormComponent
+} from "./profile-management/components/login-form/login-form.component";
+
+import {
+  CropTreatmentViewComponent
+} from "./agricultural-process/pages/crop-treatment-view/crop-treatment-view.component";
 
 export const routes: Routes = [
   { path: 'irrigation-scheduler', component: IrrigationSchedulerComponent },
   { path: 'crop-treatment-scheduler', component: CropTreatmentSchedulerComponent },
+  { path: 'crop-treatment-table', component: CropTreatmentViewComponent },
   { path: 'login', component: LoginFormComponent }
 ];
