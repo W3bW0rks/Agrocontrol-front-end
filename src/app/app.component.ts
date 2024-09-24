f59c9a71c1512da97358312757b787c113f2e975
anonimat0 <u202114714@upc.edu.pe>
2024-09-24 05:18:08 -0500

    feat: update some of information of  app archives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ import {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeAgriculturalProcessComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
