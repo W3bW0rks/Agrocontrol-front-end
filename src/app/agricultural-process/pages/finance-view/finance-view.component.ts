06002db19de6e591ca4b95c84bda61f0efde3514
Hardie Alfonso Holguin Gamarra <u202220250@upc.edu.pe>
2024-10-10 21:56:45 -0500

    feat: added finance view

---
+++ src/app/agricultural-process/pages/finance-view/finance-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject, OnInit } from '@angular/core';
+import {CropTreatment} from "../../models/crop-treatment.entity";
+import { RouterLink} from "@angular/router";
+
+
+@Component({
+  selector: 'app-finance-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './finance-view.component.html',
+  styleUrl: './finance-view.component.css'
+})
+export class FinanceViewComponent {
+
+}
